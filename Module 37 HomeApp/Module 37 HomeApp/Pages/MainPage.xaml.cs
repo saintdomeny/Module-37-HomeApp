2e51bf040935bdb1e75af7b0c056fcfb1c8f7b34
saintdomeny <daniil.nikula@gmail.com>
2023-06-12 12:12:26 +0300

    Commit

--- Module 37 HomeApp/Module 37 HomeApp/Pages/MainPage.xaml.cs
+++ Module 37 HomeApp/Module 37 HomeApp/Pages/MainPage.xaml.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace Module_37_HomeApp
+namespace Module_37_HomeApp.Pages
 {
     public partial class MainPage : ContentPage
     {
